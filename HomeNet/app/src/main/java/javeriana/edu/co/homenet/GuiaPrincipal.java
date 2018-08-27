0838c624a3f60b0729424c58f42c42ecb54c6e24
LICHO <lichobaron@gmail.com>
2018-08-26 21:17:42 -0500

    guia bases

--- HomeNet/app/src/main/java/javeriana/edu/co/homenet/GuiaPrincipal.java
+++ HomeNet/app/src/main/java/javeriana/edu/co/homenet/GuiaPrincipal.java
@@ -2,6 +2,7 @@ package javeriana.edu.co.homenet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class GuiaPrincipal extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class GuiaPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guia_principal);
+
+        getActionBar().setTitle("Menú Principal");
+        getSupportActionBar().setTitle("Menú Principal");
+
     }
 }
---
+++ HomeNet/app/src/main/java/javeriana/edu/co/homenet/GuiaSolicitudesServicios.java
@@ -0,0 +1,13 @@
+package javeriana.edu.co.homenet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GuiaSolicitudesServicios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guia_solicitudes_servicios);
+    }
+}
