7a72352f71c786bc179a1da0c94148ca7cecc2fc
AndresContreras <andres_c8@outlook.com>
2018-08-26 20:50:14 -0500

    prueba 2 anfitrion

--- HomeNet/app/src/main/java/javeriana/edu/co/homenet/MenuAnfitrion.java
+++ HomeNet/app/src/main/java/javeriana/edu/co/homenet/MenuAnfitrion.java
@@ -9,5 +9,6 @@ public class MenuAnfitrion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_anfitrion);
+        // prueba modificar archivo 
     }
 }
