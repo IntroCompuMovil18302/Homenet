f0cafdd2476d4f4f183a4e4861979c9e200137ec
ssalamanca <santiago.anime@hotmail.com>
2018-08-28 17:37:57 -0500

    Cambio en la clase ConsultarDisponibilidad

--- HomeNet/app/src/main/java/javeriana/edu/co/homenet/ConsultarDisponibilidadActivity.java
+++ HomeNet/app/src/main/java/javeriana/edu/co/homenet/ConsultarDisponibilidadActivity.java
@@ -1,13 +1,24 @@
 package javeriana.edu.co.homenet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ConsultarDisponibilidadActivity extends AppCompatActivity {
-
+    Button listo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consultar_disponibilidad);
+        listo=(Button)findViewById(R.id.button_listo);
+        listo.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(ConsultarDisponibilidadActivity.this,ReservarAlojamientoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
