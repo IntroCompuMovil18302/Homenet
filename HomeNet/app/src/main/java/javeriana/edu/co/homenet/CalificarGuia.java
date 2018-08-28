64d69649ef6860e530f82526a864954d6c089b25
andres <anfecoquin@hotmail.com>
2018-08-28 09:08:23 -0500

    Vistas relacionadas con Huésped y guía.

---
+++ HomeNet/app/src/main/java/javeriana/edu/co/homenet/CalificarGuia.java
@@ -0,0 +1,13 @@
+package javeriana.edu.co.homenet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalificarGuia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calificar_guia);
+    }
+}
