a7e8d056053b724c87da879676406685ff6d6d58
LICHO <lichobaron@gmail.com>
2018-08-28 15:31:00 -0500

    Correciones de nombre actividades guias

--- HomeNet/app/src/main/java/javeriana/edu/co/homenet/GuiaCrearAnuncioActivity.java
+++ HomeNet/app/src/main/java/javeriana/edu/co/homenet/GuiaCrearAnuncioActivity.java
@@ -3,11 +3,11 @@ package javeriana.edu.co.homenet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class GuiaHistorialServicios extends AppCompatActivity {
+public class GuiaCrearAnuncioActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_guia_historial_servicios);
+        setContentView(R.layout.activity_guia_crear_anuncio);
     }
 }
