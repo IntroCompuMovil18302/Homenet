515ac198dc3a96b85b77bafd3ac7a38402b5c730
Carlos Barón <licho_baron_leon@hotmail.com>
2018-08-28 10:27:28 -0500

    test

--- HomeNet/app/src/main/java/javeriana/edu/co/homenet/LoginActivity.java
+++ HomeNet/app/src/main/java/javeriana/edu/co/homenet/LoginActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 public class LoginActivity extends AppCompatActivity {
 
     Button registrarse;
+    Button buttonTestGuias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +23,15 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(),RegistroGeneral.class);
                 startActivity(intent);
+            }
+        });
+
+        buttonTestGuias = findViewById(R.id.buttonTestGuias);
+
+        buttonTestGuias.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(),GuiaCrearAnuncio.class));
             }
         });
 
