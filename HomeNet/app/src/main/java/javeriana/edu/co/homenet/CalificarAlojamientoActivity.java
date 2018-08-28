4e8565fd9ab7265b409c18813f1a900a23222ab0
ssalamanca <santiago.anime@hotmail.com>
2018-08-28 18:55:52 -0500

    Cambios en clases e interfaz de Huesped

--- HomeNet/app/src/main/java/javeriana/edu/co/homenet/CalificarAlojamientoActivity.java
+++ HomeNet/app/src/main/java/javeriana/edu/co/homenet/CalificarAlojamientoActivity.java
@@ -6,17 +6,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class CalificarUsuarioActivity extends AppCompatActivity {
+public class CalificarAlojamientoActivity extends AppCompatActivity {
     Button publicar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calificar_usuario);
+        setContentView(R.layout.activity_calificar_alojamiento);
         publicar=(Button)findViewById(R.id.publicar);
         publicar.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent intent = new Intent(CalificarUsuarioActivity.this,HistorialReservaActivity.class);
+                Intent intent = new Intent(CalificarAlojamientoActivity.this,HistorialReservaActivity.class);
                 startActivity(intent);
             }
         });
--- HomeNet/app/src/main/java/javeriana/edu/co/homenet/DetallesHistorialReservaActivity.java
+++ HomeNet/app/src/main/java/javeriana/edu/co/homenet/DetallesHistorialReservaActivity.java
@@ -1,13 +1,42 @@
 package javeriana.edu.co.homenet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DetallesHistorialReservaActivity extends AppCompatActivity {
-
+    Button informacion;
+    Button calificacion;
+    Button ver_ruta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalles_historial_reserva);
+        informacion =(Button)findViewById(R.id.info_alojamiento);
+        calificacion=(Button)findViewById(R.id.calificacion);
+        ver_ruta=(Button)findViewById(R.id.ver_ruta);
+        informacion.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(DetallesHistorialReservaActivity.this,InformacionAlojamientoActivity.class);
+                startActivity(intent);
+            }
+        });
+        calificacion.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(DetallesHistorialReservaActivity.this,CalificarAlojamientoActivity.class);
+                startActivity(intent);
+            }
+        });
+        ver_ruta.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(DetallesHistorialReservaActivity.this,VerComentariosActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
