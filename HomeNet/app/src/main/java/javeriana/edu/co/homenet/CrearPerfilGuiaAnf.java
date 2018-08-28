a65b4a21fb1008c1b55dcf3c482c8c5edcd0f56a
andres <anfecoquin@hotmail.com>
2018-08-27 20:11:45 -0500

    Registros y login

---
+++ HomeNet/app/src/main/java/javeriana/edu/co/homenet/CrearPerfilGuiaAnf.java
@@ -0,0 +1,13 @@
+package javeriana.edu.co.homenet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrearPerfilGuiaAnf extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_perfil_guia_anf);
+    }
+}
