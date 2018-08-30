f5f7e7a631f1dabd60046fd2ebbfa52e8ae4e5e5
LICHO <lichobaron@gmail.com>
2018-08-29 22:44:50 -0500

    casos de uso guias terminados

--- HomeNet/app/src/main/java/javeriana/edu/co/homenet/GuiaVerHuespedActivity.java
+++ HomeNet/app/src/main/java/javeriana/edu/co/homenet/GuiaVerHuespedActivity.java
@@ -1,13 +1,27 @@
 package javeriana.edu.co.homenet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GuiaVerHuespedActivity extends AppCompatActivity {
+
+    Button buttonVolverVerUserGuia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guia_ver_huesped);
+
+        buttonVolverVerUserGuia = (Button) findViewById(R.id.buttonVolverVerUserGuia);
+
+        buttonVolverVerUserGuia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(), GuiaSolicitudesServiciosActivity.class));
+            }
+        });
     }
 }
