d3d364ee19560cd77be8d844ccca6332acfc7314
andres <anfecoquin@hotmail.com>
2018-08-29 23:31:19 -0500

    Botones backeo y cositas pequeñas

--- HomeNet/app/src/main/java/javeriana/edu/co/homenet/VerInfoGuiaActivity.java
+++ HomeNet/app/src/main/java/javeriana/edu/co/homenet/VerInfoGuiaActivity.java
@@ -5,10 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class VerInfoGuiaActivity extends AppCompatActivity {
 
     Button verMasTour;
+    ImageButton back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +18,20 @@ public class VerInfoGuiaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ver_info_guia);
 
         verMasTour = findViewById(R.id.verMasTour);
+        back = findViewById(R.id.back);
 
         verMasTour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(),VerMasTourActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),GuiasDisponiblesActivity.class);
                 startActivity(intent);
             }
         });
