c058e593386910b257a7efea6ca843bbb6cb24e3
Andres Contreras <andres_c8@outlook.com>
2018-10-31 17:26:18 -0500

    cambios para galeria de img

--- HomeNet/app/src/main/java/javeriana/edu/co/homenet/activities/anfitrion/AnfitrionPublicarAlojamientoActivity.java
+++ HomeNet/app/src/main/java/javeriana/edu/co/homenet/activities/anfitrion/AnfitrionPublicarAlojamientoActivity.java
@@ -1,7 +1,9 @@
 package javeriana.edu.co.homenet.activities.anfitrion;
 
+import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,6 +16,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 
@@ -57,11 +60,13 @@ public class AnfitrionPublicarAlojamientoActivity extends AppCompatActivity impl
 
         // recyclerView
         rvImagenes.setHasFixedSize(true);
-        //rvImagenes.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rvImagenes.setLayoutManager(new LinearLayoutManager(getBaseContext()));
+
+        accionBotones();
     }
 
 
-
+    // Seccion spinner
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
         tipoAlojamiento = adapterView.getItemAtPosition(i).toString();
@@ -72,16 +77,75 @@ public class AnfitrionPublicarAlojamientoActivity extends AppCompatActivity impl
 
     }
 
-    public void accionBotones(View view) {
+    // seccion recycler view
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RESULT_LOAD_IMAGE && resultCode== RESULT_OK){ // TODO Probar si funciona sin verificar el GALLERY INTENT
+            if(data.getClipData()!=null){
+                ClipData imagenes = data.getClipData();
+                int totalItemsSelected = data.getClipData().getItemCount();
+
+                for(int i=0 ; i<totalItemsSelected;i++){
+                    ClipData.Item item = imagenes.getItemAt(i);
+                    Uri uri = item.getUri();
+                    listaImagenes.add(uri);
+                    /*
+                    StorageReference filepath = mStorage.child("Photos").child(uri.getLastPathSegment());
+                    filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
+                    */
+                }
+                mAdapter = new ImageAdapter(getActivity(),listaImagenes);
+                mRecyclerView.setAdapter(mAdapter);
+                Toast.makeText(getActivity(), "Se subieron los archivos", Toast.LENGTH_SHORT).show();
+            }else if(data.getData()!=null){
+                Uri uri = data.getData();
+                listaImagenes.add(uri);
+                mAdapter = new ImageAdapter(getActivity(),listaImagenes);
+                mRecyclerView.setAdapter(mAdapter);
+                /*
+                nProgressDialog.setMessage("Subiendo archivo...");
+                nProgressDialog.show();
+                Uri uri = data.getData();
+                StorageReference filepath = mStorage.child("Photos").child(uri.getLastPathSegment());
+                filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Toast.makeText(getActivity(), "Se subio el archivo", Toast.LENGTH_SHORT).show();
+                        nProgressDialog.dismiss();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+                */
+            }
+        }
+    }
+
+    public void accionBotones() {
         agregarImg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                System.out.println("ENTRO acccion boton");
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
                 intent.setType("image/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
                 startActivityForResult(Intent.createChooser(intent, "Seleccionar imagenes"), RESULT_LOAD_IMAGE);
-                //startActivityForResult(intent,RESULT_LOAD_IMAGE);
+                startActivityForResult(intent,RESULT_LOAD_IMAGE);
             }
         });
     }
