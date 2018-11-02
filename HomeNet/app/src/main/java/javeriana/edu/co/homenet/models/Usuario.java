442f364e7519f1642b859cc4191c7c3169bcd29c
Andres Contreras <andres_c8@outlook.com>
2018-11-01 23:52:01 -0500

    primera version clases

---
+++ HomeNet/app/src/main/java/javeriana/edu/co/homenet/models/Usuario.java
@@ -0,0 +1,75 @@
+package javeriana.edu.co.homenet.models;
+
+import java.security.PublicKey;
+
+public class Usuario {
+    private String nombre;
+    // private String contraseña;  // Opcional
+    private String urlImg;
+    private int edad;
+    private String tipoUsuario;
+    private String correo;
+    private String nacionalidad;
+    private char sexo;
+
+    public Usuario()
+    {
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUrlImg() {
+        return urlImg;
+    }
+
+    public void setUrlImg(String urlImg) {
+        this.urlImg = urlImg;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+}
