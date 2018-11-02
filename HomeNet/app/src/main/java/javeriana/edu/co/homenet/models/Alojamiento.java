9d99108cdce9d5dd6fd10d0e03de07a552b3943c
Andres Contreras <andres_c8@outlook.com>
2018-11-02 00:45:34 -0500

    cambios clases

--- HomeNet/app/src/main/java/javeriana/edu/co/homenet/models/Alojamiento.java
+++ HomeNet/app/src/main/java/javeriana/edu/co/homenet/models/Alojamiento.java
@@ -3,24 +3,76 @@ package javeriana.edu.co.homenet.models;
 import java.util.List;
 
 public class Alojamiento {
-    private List<String> urlImg;
+    private String id;
+    private List<String> urlImgs;
     private long precio;
     private String tipo;
+    private String descripcion;
 
     // relaciones
     private String anfitrion;
+    private List<Disponibilidad> disponibilidades;
+    private Ubicacion ubicacion;
 
     public Alojamiento()
     {
 
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Ubicacion getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(Ubicacion ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public List<String> getUrlImgs() {
+        return urlImgs;
+    }
+
+    public void setUrlImgs(List<String> urlImgs) {
+        this.urlImgs = urlImgs;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public List<Disponibilidad> getDisponibilidades() {
+        return disponibilidades;
+    }
+
+    public void setDisponibilidades(List<Disponibilidad> disponibilidades) {
+        this.disponibilidades = disponibilidades;
+    }
+
+    public String getAnfitrion() {
+        return anfitrion;
+    }
+
+    public void setAnfitrion(String anfitrion) {
+        this.anfitrion = anfitrion;
+    }
+
     public List<String> getUrlImg() {
-        return urlImg;
+        return urlImgs;
     }
 
     public void setUrlImg(List<String> urlImg) {
-        this.urlImg = urlImg;
+        this.urlImgs = urlImg;
     }
 
     public long getPrecio() {
