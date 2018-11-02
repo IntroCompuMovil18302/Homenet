e6ae8898b691dd7855b4f9bc39a0830c9cad0236
Andres Contreras <andres_c8@outlook.com>
2018-11-01 19:56:13 -0500

    se comentan errores

--- HomeNet/app/src/main/java/javeriana/edu/co/homenet/activities/anfitrion/AnfitrionPublicarAlojamientoActivity.java
+++ HomeNet/app/src/main/java/javeriana/edu/co/homenet/activities/anfitrion/AnfitrionPublicarAlojamientoActivity.java
@@ -66,7 +66,7 @@ public class AnfitrionPublicarAlojamientoActivity extends AppCompatActivity impl
     }
 
 
-    // Seccion spinner
+    // Seccion spinner -----------------------------------------------
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
         tipoAlojamiento = adapterView.getItemAtPosition(i).toString();
@@ -76,64 +76,14 @@ public class AnfitrionPublicarAlojamientoActivity extends AppCompatActivity impl
     public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
+    // FIN Seccion spinner -----------------------------------------------
 
-    // seccion recycler view
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == RESULT_LOAD_IMAGE && resultCode== RESULT_OK){ // TODO Probar si funciona sin verificar el GALLERY INTENT
-            if(data.getClipData()!=null){
-                ClipData imagenes = data.getClipData();
-                int totalItemsSelected = data.getClipData().getItemCount();
 
-                for(int i=0 ; i<totalItemsSelected;i++){
-                    ClipData.Item item = imagenes.getItemAt(i);
-                    Uri uri = item.getUri();
-                    listaImagenes.add(uri);
-                    /*
-                    StorageReference filepath = mStorage.child("Photos").child(uri.getLastPathSegment());
-                    filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+    // seccion recycler view --------------------------------------------
 
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
 
-                        }
-                    });
-                    */
-                }
-                mAdapter = new ImageAdapter(getActivity(),listaImagenes);
-                mRecyclerView.setAdapter(mAdapter);
-                Toast.makeText(getActivity(), "Se subieron los archivos", Toast.LENGTH_SHORT).show();
-            }else if(data.getData()!=null){
-                Uri uri = data.getData();
-                listaImagenes.add(uri);
-                mAdapter = new ImageAdapter(getActivity(),listaImagenes);
-                mRecyclerView.setAdapter(mAdapter);
-                /*
-                nProgressDialog.setMessage("Subiendo archivo...");
-                nProgressDialog.show();
-                Uri uri = data.getData();
-                StorageReference filepath = mStorage.child("Photos").child(uri.getLastPathSegment());
-                filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        Toast.makeText(getActivity(), "Se subio el archivo", Toast.LENGTH_SHORT).show();
-                        nProgressDialog.dismiss();
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
+    // FIN seccion recycler view --------------------------------------------
 
-                    }
-                });
-                */
-            }
-        }
-    }
 
     public void accionBotones() {
         agregarImg.setOnClickListener(new View.OnClickListener() {
