e9f91b5b87cb0b298419bcd5f3524e63e1c05c9e
licho <lichobaron@gmail.com>
2018-11-19 23:54:19 -0500

    iconos

--- HomeNet/app/src/main/java/javeriana/edu/co/homenet/activities/guia/GuiaCrearAnuncioBasicInfoActivity.java
+++ HomeNet/app/src/main/java/javeriana/edu/co/homenet/activities/guia/GuiaCrearAnuncioBasicInfoActivity.java
@@ -10,20 +10,11 @@ import javeriana.edu.co.homenet.R;
 
 public class GuiaCrearAnuncioBasicInfoActivity extends AppCompatActivity {
 
-    Button siguiente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guia_crear_anuncio_basic_info);
 
-        siguiente = findViewById(R.id.btSiguienteGCABA);
-
-        siguiente.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(v.getContext(),GuiaCrearAnuncioParadaActivity.class));
-            }
-        });
     }
 }
