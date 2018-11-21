130cdc2ce5900fb963771f33ee219a3f949c1b73
LICHO <lichobaron@gmail.com>
2018-11-20 23:59:22 -0500

    layouts guia

--- HomeNet/app/src/main/java/javeriana/edu/co/homenet/activities/huesped/guias/HuespedVerInfoGuiaActivity.java
+++ HomeNet/app/src/main/java/javeriana/edu/co/homenet/activities/huesped/guias/HuespedVerInfoGuiaActivity.java
@@ -5,10 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 
 import javeriana.edu.co.homenet.R;
-import javeriana.edu.co.homenet.activities.guia.GuiasDisponiblesActivity;
 
 public class HuespedVerInfoGuiaActivity extends AppCompatActivity {
 
